9b7f44c70c62bd0448cdec5312c788b0a3357251
Daniel <dgelfand@stuy.edu>
2018-02-08 16:49:04 -0500

    Modified driver; diff. size increment

--- HW06-Lab/MFDriver.java
+++ HW06-Lab/MFDriver.java
@@ -5,6 +5,7 @@
 // 2018--02--02
 
 public class MFDriver {
+    
     public static int[][] inputer(int size)
     {
 	int[][] temp = new int[size][size];
@@ -24,11 +25,22 @@ public class MFDriver {
     public static void main(String[] args)
     {
 	int[][] tester;
-	double timer;
-	for (int x = 1000; x < 2000; x += 100)
+	long currTime;
+	long endTime;
+	
+	//n x n matrix
+	for (int x = 1000; x < 10000; x += 500)
 	    {
+		//trials
+		currTime = System.nanoTime();
+		for(int j = 0; j < 1000; j++){ 
 		tester = inputer(x);
-		MatrixFinder.finder(tester, x*x - x + 1);
+		MatrixFinder.finder(tester, x*x - x + 1); //worst-case scenario
+		endTime = System.nanoTime();
+		System.out.print(x + "," + (endTime-currTime) + "\n");
+		
+		}
+	
 	    }
     }
 }
